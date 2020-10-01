b59c0677f0e3319eca469920d89f55cbac084832
Margarita Malakhova <ritulek@mail.ru>
2020-10-01 14:21:59 +0300

    feat: implement 3 functions: max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+  
+  var minNumber = array[0];
+  
+  for (var i = 1; i < array.length; i++) {
+      var currentNumber = array[i];
+      if (currentNumber < minNumber) {
+        minNumber = currentNumber;
+      }
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+      }
+      
+      var maxNumber = array[0];
+      
+      for (var i = 1; i < array.length; i++) {
+          var currentNumber = array[i];
+          if (currentNumber > maxNumber) {
+            maxNumber = currentNumber;
+          }
+      }
+      return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+      }
+      
+      var sumNumber = 0;
+      
+      for (var i = 0; i < array.length; i++) {
+          sumNumber += array[i];
+    }
+      return (sumNumber/array.length);
 }
